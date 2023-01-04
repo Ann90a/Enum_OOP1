4d6e32f5bf50aea4d53b098256bd186696479662
Ann90a <abrosimova222@yandex.ru>
2023-01-05 01:59:33 +0300

    OOP_Enum дз выполнено

---
+++ src/transport/driver/DriverBImpl.java
@@ -0,0 +1,7 @@
+package transport.driver;
+
+public class DriverBImpl extends DriverB {
+    public DriverBImpl(String fullName, boolean hasDrivingLicense, int experience) {
+        super(fullName, hasDrivingLicense, experience);
+    }
+}
